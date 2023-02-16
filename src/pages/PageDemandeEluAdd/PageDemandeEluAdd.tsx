71376a830ec4a8bdcdde8179168dc3b13d54227d
Ugen <eugene.delage@videotron.ca>
2023-02-15 21:28:53 -0500

    modifs

--- src/pages/PageDemandeEluAdd/PageDemandeEluAdd.tsx
+++ src/pages/PageDemandeEluAdd/PageDemandeEluAdd.tsx
@@ -2,7 +2,7 @@
 import Meta from "@/components/Meta";
 import { FullSizeCenteredFlexBox } from "@/components/styled";
 import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Button, Card, CardActions, CardContent, CardHeader, Divider, FormControl, Grid, MenuItem, Paper, Select, SelectChangeEvent, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
+import { Button, Card, CardActions, CardContent, CardHeader, Divider, FormControl, Grid, InputLabel, MenuItem, Paper, Select, SelectChangeEvent, Tab, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
 // react
@@ -52,61 +52,84 @@ function PageDemandeEluAdd() {
         <Box sx={{ height:'100%',width:'100%' }}>
           <Card style={{height:'100%',width:'100%'}}>
             <CardContent>
-            <Box sx={{ border: 2, borderRadius: 5,marginTop:1,maxHeight:550,minWidth: 650 }}>
-             <TabContext value={tabValue}>
-              <Box sx={{ borderBottom: 2, borderColor: 'divider' }}>
-              <TabList onChange={handleChange} aria-label="lab API tabs example">
-                <Tab label="Demande" value="1" />
-                <Tab label="Communications DG" value="2" />
-              </TabList>
-              </Box>
-              <TabPanel sx={{  height:`calc(100vh - 182px)`,width: 650}} value="1">  
-              <Box component="form"  sx={{ '& .MuiTextField-root': { m: 1, width: '25ch' },backgroundColor:'#FCFCCA' }}
-                   noValidate
-                   autoComplete="off">
-                
-                  <TextField  multiline variant="filled" 
-                   defaultValue="Tremblay, Mathieu
-                   123, rue abc
-                   Localisation de la problématique: 3, Terrasse Debien"
-                  />
-                  <TextField  multiline variant="filled" 
-                   defaultValue="Requête: RC-528-130 -Y2TB
-                   Type: INT bac brisé
-                   No Requête:"
-                  />
-                 <Box sx={{ height:'100%',width:'100%' }}>
-                 <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={equipe}
-                    label="Équipe"
-                    onChange={handleChangeEquipe}
-                    >
-                    <MenuItem value={1}>Équipe 1</MenuItem>
-                    <MenuItem value={2}>Équipe 2</MenuItem>
-                    <MenuItem value={3}>Équipe 3</MenuItem>
-                  </Select>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={typeDemande}
-                    label="Type de demande"
-                    onChange={handleChangeTypeDemande}
-                    >
-                    <MenuItem value={1}>Type demande 1</MenuItem>
-                    <MenuItem value={2}>Type demande 2</MenuItem>
-                    <MenuItem value={3}>Type demande 3</MenuItem>
-                  </Select>
-
-
-                 </Box> 
-              </Box>
+              <Box sx={{ border: 2, borderRadius: 5,marginTop:1,maxHeight:550,minWidth: 650 }}>
+                <TabContext value={tabValue}>
+                  <Box sx={{ borderBottom: 2, borderColor: 'divider' }}>
+                    <TabList onChange={handleChange} aria-label="lab API tabs example">
+                      <Tab label="Demande" value="1" />
+                      <Tab label="Communications DG" value="2" />
+                    </TabList>
+                  </Box>
+                  <TabPanel sx={{  height:`calc(100vh - 182px)`,width: 650}} value="1">  
+                     <Grid container spacing={3}>
+                        <Grid item xs={12} sm={6}>
+                           <TextField  multiline variant="filled" 
+                             fullWidth
+                            defaultValue="Tremblay, Mathieu
+                            123, rue abc
+                            Localisation de la problématique: 3, Terrasse Debien"
+                           />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                           <TextField  multiline variant="filled" 
+                             fullWidth
+                             defaultValue="Requête: RC-528-130 -Y2TB
+                             Type: INT bac brisé
+                             No Requête:"
+                           />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                          <InputLabel id="ilequipe">Équipe</InputLabel>
+                          <Select
+                              fullWidth
+                              labelId="ilequipe"
+                              id="demo-simple-select"
+                              value={equipe}
+                              label="Équipe"
+                              onChange={handleChangeEquipe}
+                              >
+                              <MenuItem value={1}>Équipe 1</MenuItem>
+                              <MenuItem value={2}>Équipe 2</MenuItem>
+                              <MenuItem value={3}>Équipe 3</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel id="iltypedemande">Type de demandes</InputLabel>
+                            <Select
+                              fullWidth
+                              labelId="iltypedemande"
+                              id="demo-simple-select"
+                              value={typeDemande}
+                              label="Type de demande"
+                              onChange={handleChangeTypeDemande}
+                              >
+                              <MenuItem value={1}>Type demande 1</MenuItem>
+                              <MenuItem value={2}>Type demande 2</MenuItem>
+                              <MenuItem value={3}>Type demande 3</MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                           <TextField  variant="filled" 
+                             fullWidth
+                             label="Statut"
+                             disabled
+                            defaultValue="Brouillon"
+                           />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                           <TextField variant="filled" 
+                             fullWidth
+                             label="Raison"
+                             disabled
+                             defaultValue="En cours"
+                           />
+                        </Grid>
+                    </Grid>
               </TabPanel>
               <TabPanel sx={{  height:`calc(100vh - 182px)`,width: 650}} value="2">
               </TabPanel>
              </TabContext>
-           </Box>      
+             </Box>      
             </CardContent>
             <CardActions sx={{
              alignSelf: "stretch",
